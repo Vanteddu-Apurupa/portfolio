5bfd3b2db638cf83d5b3f3f7ee878b945cee31d4
Vanteddu Apurupa <2200031135cseh@gmail.com>
2025-03-14 10:18:51 +0530

    updated

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -9,7 +9,7 @@ export const Education = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {/* Education Card 1 */}
           <div className="p-6 rounded-xl border border-white/10 flex items-center hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <img src="public/images/klu.jpeg" alt="University Logo" className="w-24 h-24 mr-6" />
+            <img src="/images/klu.jpeg" alt="University Logo" className="w-24 h-24 mr-6" />
             <div>
               <h3 className="text-xl font-bold mb-2">Koneru Lakshmaiah Educational Foundation</h3>
               <p className="text-gray-400 mb-2">Guntur, Andhra Pradesh</p>
@@ -24,7 +24,7 @@ export const Education = () => {
 
           {/* Education Card 2 */}
           <div className="p-6 rounded-xl border border-white/10 flex items-center hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <img src="public/images/scjc.webp" alt="College Logo" className="w-24 h-24 mr-6" />
+            <img src="/images/scjc.webp" alt="College Logo" className="w-24 h-24 mr-6" />
             <div>
               <h3 className="text-xl font-bold mb-2">Sri Chaitanya Junior College</h3>
               <p className="text-gray-400 mb-2">Hyderabad, Telangana</p>
@@ -37,7 +37,7 @@ export const Education = () => {
 
           {/* Education Card 3 (Centered Below)*/}
           <div className="p-6 rounded-xl border border-white/10 flex items-center hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition col-span-1 md:col-span-2 mx-auto">
-            <img src="public/images/SC.png" alt="School Logo" className="w-24 h-24 mr-6" />
+            <img src="/images/SC.png" alt="School Logo" className="w-24 h-24 mr-6" />
             <div>
               <h3 className="text-xl font-bold mb-2">Sri Chaitanya School</h3>
               <p className="text-gray-400 mb-2">Hyderabad, Telangana</p>
